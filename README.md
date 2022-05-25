34d022f87e7b398fd20b70ecc52700277ea5a3b4
Jeferson belmiro <jeferson.belmiro@gmail.com>
2022-05-25 11:40:37 -0300

    standardized store links

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# GetRapture
+# Get Endless Waves Survival
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.3.6.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/get-rapture'),
+      dir: require('path').join(__dirname, './coverage/get-endless-waves-survival'),
       subdir: '.',
       reporters: [
         { type: 'html' },
